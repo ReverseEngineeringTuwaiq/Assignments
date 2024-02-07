1ec982722032843fb79b5962b03633f6542715aa
Osama <example@example.com>
2024-02-07 11:06:35 +0300

    Initial Commit

---
+++ FitnessProject/FitnessProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessProjectApp.swift
+//  FitnessProject
+//
+//  Created by Osama on 26/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct FitnessProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
