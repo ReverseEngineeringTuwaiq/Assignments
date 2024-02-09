82d6608ebe9cd6af76c85581f72547e25c7113e8
vdotup <v.up@live.com>
2024-02-09 11:04:39 +0300

    Initial Commit

---
+++ grocery-app/grocery_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  grocery_appApp.swift
+//  grocery-app
+//
+//  Created by vdotup on 09/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct grocery_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
